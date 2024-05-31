cfcfae23375f201d82a81397e501e68d75981312
ArnoBidet <arno.bidet479@gmail.com>
2024-05-31 10:39:49 +0200

    refactor: Separate directional arrows controller and view

---
+++ app/controller/action_controller.py
@@ -0,0 +1,31 @@
+from model.marty import Marty
+
+class ActionController():
+		marty = Marty()
+
+		def __init__(self):
+				pass
+
+		def on_b_stand_straight(self):
+				self.marty.stand_straight()
+
+		def on_b_dance(self):
+				self.marty.dance()
+
+		def on_b_celebrate(self):
+				self.marty.celebrate()
+
+		def on_b_wave_left(self):
+				self.marty.wave_left()
+
+		def on_b_wave_right(self):
+				self.marty.wave_right()
+
+		def on_b_hello(self):
+				self.marty.hello()
+
+		def on_b_shoot_right(self):
+				self.marty.shoot_right()
+
+		def on_b_shoot_left(self):
+				self.marty.shoot_left()
