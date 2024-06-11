aba3e380fa821593de7fcf2f0b2c6338bf98659d
Nathan Toigo <nathan.toigo@iut-dijon.u-bourgogne.fr>
2024-06-11 14:28:40 +0200

    Ajout : utilisation de liste de marty à la place d'un seul dans le singleton

--- app/model/marty.py
+++ app/model/marty.py
@@ -4,10 +4,10 @@ import numpy as np
 from martypy import Marty as realMarty
 class Marty():
 	_instance = None
-	marty = None
-	marty_ip = None
-	marty_connected = False
-	marty_status = None
+	marties = [None,None]
+	marties_ip = [None,None]
+	marties_connected = [None,None]
+	marties_status = [None,None]
 	colors = {"black":0, "red":0, "green":0, "dark_blue":0, "light_blue":0, "yellow":0, "pink":0}
 	def __new__(cls, *args, **kwargs):
 		if not cls._instance:
@@ -15,80 +15,79 @@ class Marty():
 		return cls._instance
 
 
-	def go_forw(self):
-		if(self.marty_connected):
-			self.marty.walk(1,'auto',0,40,1700);
+	def go_forw(self, nbMarty=0):
+		if(self.marties_connected[nbMarty]):
+			self.marties[nbMarty].walk(1,'auto',0,40,1700)
 		else:
 			print("No marty connected")
 
-	def go_back(self):
-		self.marty.walk(1,'auto',0,-40, 1700);
+	def go_back(self, nbMarty=0):
+		self.marties[nbMarty].walk(1,'auto',0,-40, 1700)
 
-	def turn_left(self):
-		self.marty.sidestep("left")
+	def turn_left(self, nbMarty=0):
+		self.marties[nbMarty].sidestep("left")
 
-	def turn_right(self):
-		self.marty.sidestep("right")
+	def turn_right(self, nbMarty=0):
+		self.marties[nbMarty].sidestep("right")
 
-	def go_forw_left(self):
+	def go_forw_left(self, nbMarty=0):
 		for i in range(4):
-			self.marty.walk(1, 'auto', 16, 1, 1000)
+			self.marties[nbMarty].walk(1, 'auto', 16, 1, 1000)
 		self.stand_straight()
 
-	def go_forward_right(self):
+	def go_forward_right(self, nbMarty=0):
 		for i in range(4):
-			self.marty.walk(1, 'auto', -16, 1, 1000)
+			self.marties[nbMarty].walk(1, 'auto', -16, 1, 1000)
 		self.stand_straight()
 
-	def stand_straight(self):
-		if (self.marty.get_joint_position('left knee') or self.marty.get_joint_position('right knee')):
-			self.marty.walk(1, 'auto', 0, 8, 1700)
-		self.marty.stand_straight(750)
-		print(self.colors)
+	def stand_straight(self, nbMarty=0):
+		if (self.marties[nbMarty].get_joint_position('left knee') or self.marties[nbMarty].get_joint_position('right knee')):
+			self.marties[nbMarty].walk(1, 'auto', 0, 8, 1700)
+		self.marties[nbMarty].stand_straight(750)
 
-	def dance(self):
-		self.marty.dance()
+	def dance(self, nbMarty=0):
+		self.marties[nbMarty].dance()
 
-	def celebrate(self):
-		self.marty.celebrate()
+	def celebrate(self, nbMarty=0):
+		self.marties[nbMarty].celebrate()
 
-	def wave_left(self):
-		self.marty.wave("left")
+	def wave_left(self, nbMarty=0):
+		self.marties[nbMarty].wave("left")
 
-	def wave_right(self):
-		self.marty.wave("right")
+	def wave_right(self, nbMarty=0):
+		self.marties[nbMarty].wave("right")
 
-	def hello(self):
-		self.marty.hello()
+	def hello(self, nbMarty=0):
+		self.marties[nbMarty].hello()
 
-	def shoot_right(self):
-		self.marty.kick("right")
+	def shoot_right(self, nbMarty=0):
+		self.marties[nbMarty].kick("right")
 
-	def shoot_left(self):
-		self.marty.kick("left")
+	def shoot_left(self, nbMarty=0):
+		self.marties[nbMarty].kick("left")
 
-	def hi(self):
-		self.marty.stand_straight(200, blocking=False)
-		self.marty.eyes(pose_or_angle=-20, move_time=100, blocking=False)
-		self.marty.arms(left_angle=0, right_angle=120, move_time=200)
-		self.marty.arms(left_angle=0, right_angle=0, move_time=200, blocking=False)
-		self.marty.eyes(pose_or_angle=0, move_time=100)
+	def hi(self, nbMarty=0):
+		self.marties[nbMarty].stand_straight(200, blocking=False)
+		self.marties[nbMarty].eyes(pose_or_angle=-20, move_time=100, blocking=False)
+		self.marties[nbMarty].arms(left_angle=0, right_angle=120, move_time=200)
+		self.marties[nbMarty].arms(left_angle=0, right_angle=0, move_time=200, blocking=False)
+		self.marties[nbMarty].eyes(pose_or_angle=0, move_time=100)
 
-	def connect_marty(self,ip):
+	def connect_marty(self,ip,nbMarty=0):
 		try:
-			self.marty = realMarty("wifi", ip, blocking=False)
-			self.marty_connected = True
-			self.marty.get_ready()
+			self.marties[nbMarty] = realMarty("wifi", ip, blocking=False)
+			self.marties_connected[nbMarty] = True
+			self.marties[nbMarty].get_ready()
 			return "success"
 		except:
-			self.marty = None
-			self.marty_connected = False
+			self.marties[nbMarty] = None
+			self.marties_connected[nbMarty] = False
 			return "failed"
 
-	def color_calibration(self, color:str):
+	def color_calibration(self, color:str, nbMarty=0):
 		mean = 0
 		for i in range(15):
-			mean = mean + int(self.marty.get_color_sensor_hex('left'), 16)
+			mean = mean + int(self.marties[0].get_color_sensor_hex('left'), 16)
 		self.colors[color] = mean / 15
 
 
